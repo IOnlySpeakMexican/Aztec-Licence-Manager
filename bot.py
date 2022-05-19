30a71339f6ac96d8f23b21333bb79844885a2fb8
Howl <93114188+IOnlySpeakMexican@users.noreply.github.com>
2022-05-19 14:24:04 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,23 @@
 
 import discord
-from discord import  Role, ui, app_commands
+from discord import  Guild, Interaction, Role, ui, app_commands
 from datetime import datetime
 import requests
 import secrets
 import sqlite3
 import datetime
 from discord.ext import commands, tasks
-
+import discord
+from discord.utils import get
 connection = sqlite3.connect("Users.db")
 cur = connection.cursor()
 connection.row_factory = lambda cursor, row: row[0]
+intents = discord.Intents.all()  
+intents.members = True
 
 class client(discord.Client):
     def __init__(self) -> None:
-        super().__init__(intents = discord.Intents.default())
+        super().__init__(intents = intents)
         self.synced = False
     async def on_ready(self):
         await self.wait_until_ready()
@@ -46,7 +49,7 @@ class claimkey(ui.Modal, title="Licence Redemption"):
         data = cur.fetchone()
         if data:
             query = cur.execute(f"DELETE FROM Keys WHERE Keys.Key = '{self.key}'")
-            cur.execute(f"INSERT INTO Users (Licence, User, Role, Expires) VALUES ('{self.key}','{interaction.user}', '{data[2]}', '{data[1]}')")
+            cur.execute(f"INSERT INTO Users (Licence, User, Role, Expires) VALUES ('{self.key}','{interaction.user.id}', '{data[2]}', '{data[1]}')")
             role = interaction.guild.get_role(data[2])
             await interaction.user.add_roles(role)
             connection.commit()
@@ -62,8 +65,13 @@ async def license_check():
     cur.execute(f"SELECT * FROM Users WHERE Users.Expires = '{datetime.date.today()}'")
     res = cur.fetchone()
     if res:
+        guild = aclient.get_guild(884257488201981962)
+        member = guild.get_member(int(res[1]))
+        print(res[2])
+        role = get(guild.roles, id=int(res[2]))
+        await member.remove_roles(role)
         cur.execute(f"DELETE FROM Users WHERE Users.Licence = '{res[0]}'")
-        print("deleted from users table")
+        connection.commit()
     else:
         cur.execute(f"SELECT * FROM Keys WHERE Keys.Exipry = '{datetime.date.today()}'")
         ress = cur.fetchone()
@@ -72,7 +80,6 @@ async def license_check():
             connection.commit()
             print("deleted from key table")
 
-    
 
 aclient = client()
 tree = app_commands.CommandTree(aclient)
@@ -85,4 +92,4 @@ async def generate(interaction: discord.Interaction):
 async def redeem(interaction: discord.Interaction):
     await interaction.response.send_modal(claimkey())
 
-aclient.run("OTcxNTEyMjkzMjMxODUzNjEw.GM42ph.s2lCCXsN7-i6M4xhJHprfUoRT1tE07NrSD6zVU")
+aclient.run("")
